9d36c102afcf73db5661fdde18d19e7fa0c51387
Michal Borsky <Michal.Borsky@protonmail.com>
2024-10-18 21:40:27 +0100

    Update train_transformer.py

--- train_transformer.py
+++ train_transformer.py
@@ -123,42 +123,46 @@ def train_epoch(
             }
         )
 
+def main():
+    morpho = MorphoDataset("czech_pdt")
+    
+    args = {
+        "batch_size": 128,
+        "epochs": 10,
+        "device": "cuda" if torch.cuda.is_available() else "cpu",
+        "dataset": "czech_pdt",
+        "model": "transformer",
+        "heads": 8,
+        "input_dropout": 0,
+        "model_dim": 512,
+        "keys_dim": 512,
+        "values_dim": 512,
+        "max_seq_len": morpho.max_length,
+        "encoder_stack_size": 2,
+        "decoder_stack_size": 2,
+        "input_vocab_size": morpho.train.unique_words,
+        "num_classes": morpho.train.unique_tags,
+        "label_smoothing": 0.1,
+        "packed_sequences": False,
+        "characters": False,
+    }
+    
+    
+    model = Transformer(args)
+    optim = torch.optim.AdamW(model.parameters(), 0.00075)
+    loss_fn = nn.CrossEntropyLoss(label_smoothing=args["label_smoothing"])
+    train_dloader = morpho.train.to_dataloader(args["batch_size"], shuffle=True)
+    dev_dloader = morpho.dev.to_dataloader(args["batch_size"], shuffle=False)
+    scheduler = StepLR(optim, step_size=1, gamma=0.75)
+    
+    wandb.login()
+    run_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    wandb.init(project="tagger_competition", name=run_name, config=args)
+    for epoch in range(args["epochs"]):
+        train_epoch(model, train_dloader, dev_dloader, loss_fn, optim, scheduler, None)
+    wandb.finish()
 
 
-morpho = MorphoDataset("czech_pdt")
+if __name__ == "__main__":
+    main()
 
-args = {
-    "batch_size": 128,
-    "epochs": 10,
-    "device": "cuda" if torch.cuda.is_available() else "cpu",
-    "dataset": "czech_pdt",
-    "model": "transformer",
-    "heads": 8,
-    "input_dropout": 0,
-    "model_dim": 512,
-    "keys_dim": 512,
-    "values_dim": 512,
-    "max_seq_len": morpho.max_length,
-    "encoder_stack_size": 2,
-    "decoder_stack_size": 2,
-    "input_vocab_size": morpho.train.unique_words,
-    "num_classes": morpho.train.unique_tags,
-    "label_smoothing": 0.1,
-    "packed_sequences": False,
-    "characters": False,
-}
-
-
-model = Transformer(args)
-optim = torch.optim.AdamW(model.parameters(), 0.00075)
-loss_fn = nn.CrossEntropyLoss(label_smoothing=args["label_smoothing"])
-train_dloader = morpho.train.to_dataloader(args["batch_size"], shuffle=True)
-dev_dloader = morpho.dev.to_dataloader(args["batch_size"], shuffle=False)
-scheduler = StepLR(optim, step_size=1, gamma=0.75)
-
-wandb.login()
-run_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-wandb.init(project="tagger_competition", name=run_name, config=args)
-for epoch in range(args["epochs"]):
-    train_epoch(model, train_dloader, dev_dloader, loss_fn, optim, scheduler, None)
-wandb.finish()
